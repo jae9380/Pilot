563bd1918b5f14651ac0c56a0c0fcf89dd9b5afe
jae9380 <ljy5314@gmail.com>
2025-05-15 16:18:21 +0900

    Process02 / Problem 07

--- test/src/main/java/com/step2/RoadToBiodome06.java
+++ test/src/main/java/com/step2/RoadToBiodome06.java
@@ -38,20 +38,10 @@ public class RoadToBiodome06 {
         System.out.printf("Mean : %1$.1f, Median : %2$.1f \nBonus Mean : %3$.1f, Median : %4$.1f",
                 getAverage(arr1, arr2, false), findMedian(arr1, arr2, false),
                 getAverage(arr1, arr2, true), findMedian(arr1, arr2, true));
-        System.out.printf("");
     }
 
 //    중앙값과 평균을 반환하는 메서드에서 boolean타입의 매개값을 통해서 해당 값을 구할 때 보너스 문제 여부를 판별하도록 작성했다.
-
-/*
-    아래의 메서드는 이중탐색 알고리즘을 이용하여 중간값을 반환하도록 했다.
-    메서드의 매개값으로 두 배열이 온다. 이 때 짧은 배열을 기준으로 이중탐색을 진행해야 결과값을 보다 빨리 받을 수 있기 때문에 조건문을 사용하여 짧은 배열이 첫 매개값이 되도록 했다.
-
-    각 배열을 관리하기 위해서 변수 i와 j를 선언하였고, i는 첫 매개 배열을 관리한다. 그리고 j는 나머지 배열을 관리하는데 사용이 된다.
-
-    이렇게 i와 j를 이용하여 떨어진 두 배열에서 가상의 파티션을 두어 구분하기 위해서 사용이 되는데 그렇기 떄문에 i의 초기화 값은 arr1의 중간 인덱스이고, j는 전체 배열의 i값을 뺸 값으로 초기화를 한다.
- */
-    public static double findMedian(int[] arr1, int[] arr2, boolean isItBonus) {
+    public static double findMedian(int[] arr1, int[] arr2, boolean isItBonus) { // 코드 설명은 따로 작성
         if (!isItBonus && arr1.length > arr2.length) return findMedian(arr2, arr1, false); // 더 짧은 배열을 기준으로 이진탐색 진행하기 위함
 
         if (isItBonus) {
@@ -66,36 +56,14 @@ public class RoadToBiodome06 {
         int iMin = 0, iMax = n;
         int halfLen = (n + m + 1) / 2;
 
-        System.out.println("--- arr1 배열 ---");
-        for (int i : arr1) {
-            System.out.print(i+" ");
-        }
-        System.out.println();
-        System.out.println("--- arr2 배열 ---");
-        for (int i : arr2) {
-            System.out.print(i+" ");
-        }
-
-        System.out.println("이진 탐색 진행");
-        int count = 0;
         while (iMin <= iMax) {
-            System.out.println("----"+ ++count+"회 동작 ----");
             int i = (iMin + iMax) / 2;
             int j = halfLen - i;
-            System.out.println("i = "+i+" / j = "+j);
             if (i < n && arr2[j - 1] > arr1[i]) {
-                System.out.println("arr2[j - 1] = "+arr2[j - 1]+" / arr1[i] = "+arr1[i]);
-                System.out.println("i 는 n보다 작고, arr2[j - 1]은  arr1[i] 보다 크다.");
                 iMin = i + 1;
-                System.out.println("i값 1 증가");
-
             } else if (i > 0 && arr1[i - 1] > arr2[j]) {
-                System.out.println("arr1[i - 1] = "+arr1[i - 1]+" / arr2[j] = "+arr2[j]);
-                System.out.println("i 는 0보다 크고, arr1[i - 1]은  arr2[j] 보다 크다.");
                 iMax = i - 1;
-                System.out.println("i값 1 감소");
             } else {
-                // 위 두 조건문을 통해 인덱스를 옮겨 중간값의 인덱스를 찾음
                 int maxOfLeft;
                 if (i == 0) maxOfLeft = arr2[j - 1];
                 else if (j == 0) maxOfLeft = arr1[i - 1];
