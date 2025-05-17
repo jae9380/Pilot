f707aef096408ebece03ca4c283c49fbc1007e2d
jae9380 <ljy5314@gmail.com>
2025-05-17 10:24:32 +0900

    Process 02 / Problem 08 - Bonus

--- test/src/main/java/com/step2/RoadToBiodome08.java
+++ test/src/main/java/com/step2/RoadToBiodome08.java
@@ -1,8 +1,10 @@
 package com.step2;
 
 
+import java.util.Arrays;
+
 public class RoadToBiodome08 {
-    static Integer[] customQueue = new Integer[100];
+    static Integer[] customQueue = new Integer[10];
     static int front = 0, rear = 0, size = 0;
     public static void main(String[] args) {
 
@@ -18,8 +20,14 @@ public class RoadToBiodome08 {
     }
 
     static boolean enqueue(Integer value) { // .offer(type value);
-        if (rear < 0 || rear > 100 - 1){
+        if (rear < 0 || rear > 99){
             return false;
+        }
+
+//      기본 크기 10, 동적으로 크기를 10씩 늘려감
+        if ((rear+1) % 10 == 0) {
+            customQueue = Arrays.copyOf(customQueue, customQueue.length + 10);
+            System.out.printf("Queue의 크기가 %d으로 늘어났습니다.\n", customQueue.length);
         }
         customQueue[rear++] = value;
         size++;
